4097dba25314ab780a8e0bd381d700c75f5ae882
glixzzy <itrxpzz0@gmail.com>
2021-12-27 14:14:06 -0600

    working with prompts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const prompts = require('prompts');
 
 const main = require('./main');
 
+const choices = require('./assets/choices');
 const questions = require('./assets/prompts');
 const calculations = require('./assets/calculations');
 
@@ -10,14 +11,21 @@ const getUpStreamNumber = require('./utils/getUpStreamNumber');
 const getGameVersion = require('./utils/getGameVersion');
 
 (async () => {
-    const response = await prompts(questions);
+    const response = await prompts(choices);
 
-    const gamePin = response.gamePin
-    const upstreamNumber = await getUpStreamNumber(gamePin)
-    const gameVersion = calculations.calc(await getGameVersion(gamePin))
-    const token = await getToken();
-    const botName = response.username;
+    if (response.func.length == 0) {
+        return console.log('No choices selected!'.red)
+    } else if (response.func.length > 1) {
+        return console.log('You can only pick one function at a time'.red);
+    } else if (response.func[0] == 'main') {
+        const response = await prompts(questions);
+        
+        const gamePin = response.gamePin
+        const upstreamNumber = await getUpStreamNumber(gamePin);
+        const gameVersion = calculations.calc(await getGameVersion(gamePin));
+        const token = await getToken();
+        const botName = response.username;
 
-    main.run(upstreamNumber, gameVersion, gamePin, token, botName);
-    
+        main.run(upstreamNumber, gameVersion, gamePin, token, botName);
+    }
 })();
