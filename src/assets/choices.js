41f9a3824e3dabbab931b54227341bb9d9822cda
glixzzy <itrxpzz0@gmail.com>
2021-12-27 14:13:19 -0600

    choices

---
+++ src/assets/choices.js
@@ -0,0 +1,12 @@
+const choices = [
+    {
+        type: 'multiselect',
+        name: 'func',
+        message: 'Pick a function to use',
+        choices: [
+            { title: 'Join Game', description: 'Joins a live game', value: 'main' },
+        ],
+    }
+];
+
+module.exports = choices
