b15613b8d57fc64a208f6693ee79d9293b18a163
glixzzy <itrxpzz0@gmail.com>
2021-12-27 14:52:11 -0600

    error handler

--- src/utils/getUpStreamNumber.js
+++ src/utils/getUpStreamNumber.js
@@ -9,6 +9,8 @@ async function getUpStreamNumber(gamePin) {
 
     const response = await axios.get(`https://quizlet.com/webapi/3.2/game-instances?filters=${encodeURIComponent(JSON.stringify(filters))}&perPage=500`);
 
+    if (response.data.responses[0].models.gameInstance[0] == undefined) return console.log('Game not found!'.red);
+
     return response.data.responses[0].models.gameInstance[0].upstreamNumber
 }
 
