d65223f226a57d3b38e18f00034feabce7199b14
glixzzy <itrxpzz0@gmail.com>
2021-12-27 16:45:05 -0600

    added new choice

--- src/assets/choices.js
+++ src/assets/choices.js
@@ -5,6 +5,7 @@ const choices = [
         message: 'Pick a function to use',
         choices: [
             { title: 'Join Game', description: 'Joins a live game', value: 'main' },
+            { title: 'Flood Game', description: 'Floods a live game', value: 'flood' },
         ],
     }
 ];
