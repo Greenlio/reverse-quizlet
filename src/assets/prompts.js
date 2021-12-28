966301ce627a4a1287ccc856fa768c512faa6c10
glixzzy <itrxpzz0@gmail.com>
2021-12-28 15:25:30 -0600

    add validations

--- src/assets/prompts.js
+++ src/assets/prompts.js
@@ -3,11 +3,13 @@ const mainQuestions = [
         type: 'number',
         name: 'gamePin',
         message: 'Enter Game Pin: ',
+        validate: gamePin => gamePin == "" ? `You did not enter a game pin!` : true
     },
     {
         type: 'text',
         name: 'username',
         message: 'Enter Bot Name: ',
+        validate: username => username == "" ? `You did not enter a bot name!` : true
     },
 ];
 
@@ -16,11 +18,13 @@ const floodQuestions = [
         type: 'number',
         name: 'gamePin',
         message: 'Enter Game Pin: ',
+        validate: gamePin => gamePin == "" ? `You did not enter a game pin!` : true
     },
     {
         type: 'number',
         name: 'floodAmount',
         message: 'How many bots do you want to send?: ',
+        validate: floodAmount => floodAmount == "" ? `You did not specify how many bots you want to send!` : true
     },
 ];
 
